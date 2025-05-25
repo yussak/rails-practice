5ff1ed387de3198041ea4e9546b25a286024ed36
yussak <yusuke.sakuraba0213@gmail.com>
2025-05-25 19:19:57 +0900

    サンプルフォーム追加

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -2,4 +2,8 @@ class SampleController < ApplicationController
   def index
     # hello_react.jsxを呼ぶ用
   end
+
+  def erb_only
+    # erb_only.html.erbを呼ぶ用
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'hello', to: 'sample#index'
+
+  get "erb_only", to: "sample#erb_only"
 end
