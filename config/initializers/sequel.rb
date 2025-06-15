afa6b41028bd7dc10d2c2ed1faef53d805a97d91
yussak <yusuke.sakuraba0213@gmail.com>
2025-06-15 12:34:45 +0900

    テスト用DB用意

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,13 +1,20 @@
-host = ENV["DATABASE_HOST"]
-database = ENV["DATABASE_NAME"]
-user = ENV["DATABASE_USER"]
-password = ENV["DATABASE_PASSWORD"]
-
-DB = Sequel.connect(
-  adapter: 'postgres',
-  host: ENV['DATABASE_HOST'],
-  port: ENV['DATABASE_PORT'],
-  user: ENV['DATABASE_USER'],
-  password: ENV['DATABASE_PASSWORD'],
-  database: ENV['DATABASE_NAME']
-)
+# Configure database connection based on environment
+if Rails.env.test?
+  DB = Sequel.connect(
+    adapter: 'postgres',
+    host: 'test_db',
+    port: 5432,
+    user: 'postgres',
+    password: 'password',
+    database: 'myapp_test'
+  )
+else
+  DB = Sequel.connect(
+    adapter: 'postgres',
+    host: ENV['DATABASE_HOST'],
+    port: ENV['DATABASE_PORT'],
+    user: ENV['DATABASE_USER'],
+    password: ENV['DATABASE_PASSWORD'],
+    database: ENV['DATABASE_NAME']
+  )
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,11 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+# Disable ActiveRecord fixtures
+RSpec.configure do |config|
+  config.use_active_record = false
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,18 +35,8 @@ require 'rspec/rails'
 # rescue ActiveRecord::PendingMigrationError => e
 #   abort e.to_s.strip
 # end
+
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
-
-  # You can uncomment this line to turn off ActiveRecord support entirely.
-  config.use_active_record = false
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
